875e194fedd3f0d8956caf3b74fddec94f8f14cf
lihao <ablelh@163.com>
2020-11-30 18:23:01 +0800

    no message

--- SwiftLeetCode/SwiftLeetCode/main.swift
+++ SwiftLeetCode/SwiftLeetCode/main.swift
@@ -9,3 +9,36 @@ import Foundation
 
 print("Hello, World!")
 
+
+//Definition for a binary tree node.
+  public class TreeNode {
+      public var val: Int
+      public var left: TreeNode?
+      public var right: TreeNode?
+      public init(_ val: Int) {
+          self.val = val
+          self.left = nil
+          self.right = nil
+      }
+  }
+
+
+let solution = Solution()
+
+let node1:TreeNode = TreeNode(1);
+let node2:TreeNode = TreeNode(2);
+let node3:TreeNode = TreeNode(3);
+let node4:TreeNode = TreeNode(4);
+let node5:TreeNode = TreeNode(5);
+let node6:TreeNode = TreeNode(6);
+let node7:TreeNode = TreeNode(7);
+let node8:TreeNode = TreeNode(8);
+node1.left = node2
+node1.right = node3
+node2.left = node4
+node2.right = node5
+node3.left = node6
+node3.right = node7
+node4.left = node8
+solution.mirrorTree(node1)
+ 
---
+++ SwiftLeetCode/SwiftLeetCode/剑指 Offer 27_二叉树的镜像.swift
@@ -0,0 +1,73 @@
+//
+//  剑指 Offer 27_二叉树的镜像.swift
+//  SwiftLeetCode
+//
+//  Created by lihao on 2020/11/30.
+//
+
+//剑指 Offer 27. 二叉树的镜像
+//请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+//
+//例如输入：
+//
+//     4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9
+//镜像输出：
+//
+//     4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1
+//
+//
+//
+//示例 1：
+//
+//输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+
+
+import Foundation
+
+
+class Solution {
+    func mirrorTree(_ root: TreeNode?) -> TreeNode? {
+
+        //层级遍历
+        
+        var queue:Array = Array<Any>()
+        
+        if (root == nil) {
+            return root
+        }
+       
+        queue.append(root!)
+        while (queue.count != 0 ) {
+            
+         let node:TreeNode = queue.remove(at: 0) as! TreeNode;
+        
+         print(node.val)
+    
+        let temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    
+        if (node.left != nil ) {
+            
+            queue.append(node.left!);
+        }
+            
+        if (node.right != nil ) {
+            queue.append(node.right!);
+        }
+            
+    }
+        
+        
+        return root
+    }
+}
